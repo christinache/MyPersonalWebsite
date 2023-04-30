39165f27a6c2947bfedda8eaad54fe2ed0d93bd2
christinache <christinachen0325@gmail.com>
2023-04-29 23:45:49 -0400

    Project Update

---
+++ javascripts/index.js
@@ -0,0 +1,8 @@
+//src="https://unpkg.com/typed.js@2.0.15/dist/typed.umd.js"
+
+//const typed new Typed(".auto-type", {
+    //strings: ["Product Manager", "Project Manager", "Program Manager"],
+    //typeSpeed: 150,
+    //backSpeed: 150,
+    //loop: true,
+//});
